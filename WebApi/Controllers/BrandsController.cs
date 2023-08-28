8d794b0dfa183ddc2c6bc969cf81de20cece500a
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2023-08-28 16:11:05 +0300

    Added BrandsController

---
+++ WebApi/Controllers/BrandsController.cs
@@ -0,0 +1,17 @@
+﻿using Application.Features.Brands.Commands.Create;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BrandsController : BaseController
+{
+    [HttpPost]
+    public async Task<IActionResult> Add([FromBody] CreateBrandCommand createBrandCommand) 
+    {
+        CreatedBrandResponse response = await Mediator.Send(createBrandCommand);
+        return Ok(response);
+    }
+}
