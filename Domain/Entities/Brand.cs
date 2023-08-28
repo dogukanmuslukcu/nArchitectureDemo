00f9ba9a037d1ea470d0f096bbd995cd1f4ce8ae
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2023-08-28 15:17:11 +0300

    Brand Modified

---
+++ Domain/Entities/Brand.cs
@@ -0,0 +1,23 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+
+public class Brand: Entity<Guid>
+{
+    public string Name { get; set; }
+    public Brand()
+    {
+        
+    }
+    public Brand(Guid id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+}
