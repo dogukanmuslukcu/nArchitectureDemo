fcbd92ff69231b3b0a3fd6d018a1861f7e5baff7
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2023-09-11 14:39:25 +0300

    Brand Response Modified

--- Application/Features/Brands/Commands/Create/CreatedBrandResponse.cs
+++ Application/Features/Brands/Commands/Create/CreatedBrandResponse.cs
@@ -10,4 +10,5 @@ public class CreatedBrandResponse
 {
     public Guid Id { get; set; }
     public string Name { get; set;}
+    public DateTime CreatedDate { get; set; }
 }
--- Application/Features/Brands/Commands/Update/UpdateBrandResponse.cs
+++ Application/Features/Brands/Commands/Update/UpdateBrandResponse.cs
@@ -10,5 +10,7 @@ public class UpdatedBrandResponse
 {
     public  Guid Id { get; set; }
     public string Name { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
 
 }
