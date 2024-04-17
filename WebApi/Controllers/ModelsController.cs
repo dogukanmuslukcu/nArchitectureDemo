b463795cb621c31cbc548224efdc6901b966358f
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2024-04-17 18:21:22 +0300

    Model query include implemented

---
+++ WebApi/Controllers/ModelsController.cs
@@ -0,0 +1,21 @@
+﻿using Application.Features.Models.Queries.GetList;
+using Core.Application.Requests;
+using Core.Application.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModelsController : BaseController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetListModelQuery getListBrandQuery = new() { PageRequest = pageRequest };
+            GetListResponse<GetListModelListItemDto> response = await Mediator.Send(getListBrandQuery);
+            return Ok(response);
+        }
+    }
+}
