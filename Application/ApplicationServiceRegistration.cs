d1b3289b82e16318008193ec2fa3f625a0469547
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2024-04-24 12:46:56 +0300

    SubClass of type IOC implemented

--- Application/ApplicationServiceRegistration.cs
+++ Application/ApplicationServiceRegistration.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Core.Application.Rules;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,6 +19,31 @@ public static class ApplicationServiceRegistration
         {
             configuration.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
         });
+        services.AddSubClassesOfType(Assembly.GetExecutingAssembly(), typeof(BaseBusinessRules));
+        
+        return services;
+    }
+    public static IServiceCollection AddSubClassesOfType
+        (
+        this IServiceCollection services,
+        Assembly assembly,
+        Type type,
+        Func<IServiceCollection, Type, IServiceCollection>? addWithLifeCycle = null
+        )
+    {
+        var types = assembly.GetTypes().Where(t=>t.IsSubclassOf(type) && type != t).ToList();
+        foreach (var item in types) 
+        {
+            if (addWithLifeCycle == null)
+            {
+                services.AddScoped(item);
+            }
+            else 
+            {
+                addWithLifeCycle(services, type);
+            }
+            
+        }
         return services;
     }
 }
--- Application/Features/Brands/Rules/BrandBusinessRules.cs
+++ Application/Features/Brands/Rules/BrandBusinessRules.cs
@@ -1,16 +1,13 @@
-﻿using Application.Features.Brands.Constants;
+﻿
+using Application.Features.Brands.Constants;
 using Application.Services.Repositories;
+using Core.Application.Rules;
 using Core.CrossCuttingConcerns.Exceptions.Types;
 using Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Features.Brands.Rules;
 
-public class BrandBusinessRules
+public class BrandBusinessRules:BaseBusinessRules
 {
     private readonly IBrandRepository _brandRepository;
 
