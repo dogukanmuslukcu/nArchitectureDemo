98c8f9c4e54e327bbf4f9d354a180edc942dadfc
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2024-04-24 11:51:01 +0300

    Brand Business  Rule for  BrandNameCannotBeDuplicatedWhenInserted() added.

---
+++ Application/Features/Brands/Rules/BrandBusinessRules.cs
@@ -0,0 +1,29 @@
+﻿using Application.Features.Brands.Constants;
+using Application.Services.Repositories;
+using Core.CrossCuttingConcerns.Exceptions.Types;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Rules;
+
+public class BrandBusinessRules
+{
+    private readonly IBrandRepository _brandRepository;
+
+    public BrandBusinessRules(IBrandRepository brandRepository)
+    {
+        _brandRepository = brandRepository;
+    }
+    public async Task BrandNameCannotBeDuplicatedWhenInserted(string name) 
+    {
+        Brand? result = await _brandRepository.GetAsync(predicate: b=>b.Name.ToLower() == name.ToLower());
+        if (result != null) 
+        {
+            throw new BusinessException(BrandsMessages.BrandNameExists);
+        }
+    }
+}
