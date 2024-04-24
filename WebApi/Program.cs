158bd86c28ddc6b01b725c03d1e57da484d0665a
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2024-04-24 15:13:39 +0300

    Added exceptionMiddleware program.cs

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,4 +1,5 @@
 using Application;
+using Core.CrossCuttingConcerns.Exceptions.Extensions;
 using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,6 +21,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+//if(app.Environment.IsProduction())
+app.ConfigureCustomExceptionMiddleware();
 
 app.UseHttpsRedirection();
 
