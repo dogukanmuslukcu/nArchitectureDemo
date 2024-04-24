c6c1dc460ca5d26f30200e453e6e771206f14180
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2024-04-24 12:19:28 +0300

    _brandBusinessRules injected createdBrandResponse Handle

--- Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
+++ Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -29,6 +29,8 @@ public class CreateBrandCommand : IRequest<CreatedBrandResponse>
 
         public async Task<CreatedBrandResponse>? Handle(CreateBrandCommand request, CancellationToken cancellationToken)
         {
+            await _brandBusinessRules.BrandNameCannotBeDuplicatedWhenInserted(request.Name);
+
             Brand brand = _mapper.Map<Brand>(request);
             brand.Id = Guid.NewGuid();
 
