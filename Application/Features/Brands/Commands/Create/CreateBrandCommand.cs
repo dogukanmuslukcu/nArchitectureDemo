a1aa0280c374a8228e38012815d561b464d2e2e6
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2024-04-24 11:56:31 +0300

    Correction Task<CreatedBrandResponse>? Handle

--- Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
+++ Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -1,4 +1,5 @@
-﻿using Application.Services.Repositories;
+﻿using Application.Features.Brands.Rules;
+using Application.Services.Repositories;
 using AutoMapper;
 using Domain.Entities;
 using MediatR;
@@ -17,20 +18,23 @@ public class CreateBrandCommand : IRequest<CreatedBrandResponse>
     {
         private readonly IBrandRepository _brandRepository;
         private readonly IMapper _mapper;
+        private readonly BrandBusinessRules _brandBusinessRules;
 
-        public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper)
+        public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper, BrandBusinessRules brandBusinessRules)
         {
             _brandRepository = brandRepository;
             _mapper = mapper;
+            _brandBusinessRules = brandBusinessRules;
         }
 
         public async Task<CreatedBrandResponse>? Handle(CreateBrandCommand request, CancellationToken cancellationToken)
         {
             Brand brand = _mapper.Map<Brand>(request);
             brand.Id = Guid.NewGuid();
-            var result = await _brandRepository.AddAsync(brand);
 
-            CreatedBrandResponse response = _mapper.Map<CreatedBrandResponse>(result);
+            await _brandRepository.AddAsync(brand);
+
+            CreatedBrandResponse response = _mapper.Map<CreatedBrandResponse>(brand);
             return response;
         }
     }
