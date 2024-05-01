3341342441ac83cf705ba5deaa97fc081960ba67
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2024-05-01 15:23:17 +0300

    ICacheRemoverRequest implemented

--- Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
+++ Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -1,6 +1,7 @@
 ﻿using Application.Features.Brands.Rules;
 using Application.Services.Repositories;
 using AutoMapper;
+using Core.Application.Pipelines.Caching;
 using Core.Application.Pipelines.Transaction;
 using Domain.Entities;
 using MediatR;
@@ -12,9 +13,14 @@ using System.Threading.Tasks;
 
 namespace Application.Features.Brands.Commands.Create;
 
-public class CreateBrandCommand : IRequest<CreatedBrandResponse>,ITransactionalRequest
+public class CreateBrandCommand : IRequest<CreatedBrandResponse>,ITransactionalRequest,ICacheRemoverRequest
 {
     public string Name { get; set; }
+
+    public string CacheKey => throw new NotImplementedException();
+
+    public bool BypassCache => throw new NotImplementedException();
+
     public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
     {
         private readonly IBrandRepository _brandRepository;
