3b1f795555c04f0e4a41a1db828a4a136f78ac03
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2024-05-29 12:40:44 +0300

    Entity configuration for db created to CorePackages.Security entitiy objects

--- Persistence/Contexts/BaseDbContext.cs
+++ Persistence/Contexts/BaseDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Domain.Entities;
+﻿using Core.Security.Entities;
+using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System;
@@ -19,6 +20,12 @@ public class BaseDbContext : DbContext
     public DbSet<Transmission> Transmissions { get; set; }
     public DbSet<Model> Models { get; set; }
 
+    public DbSet<OperationClaim> OperationClaims { get; set; }
+    public DbSet<OtpAuthenticator> OtpAuthenticators { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+    public DbSet<EmailAuthenticator> EmailAuthenticators { get; set; }
 
     public BaseDbContext(DbContextOptions dbContextOptions, IConfiguration configuration) : base(dbContextOptions)
     {
